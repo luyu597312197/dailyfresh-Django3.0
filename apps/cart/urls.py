75e651dfba3faff2c0901a4cb459afb8bd0e63a9
luyu597312197 <58407585+luyu597312197@users.noreply.github.com>
2020-07-23 11:09:54 +0800

    Add files via upload

---
+++ apps/cart/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from cart.views import CartAddView, CartInfoView, CartUpdateView, CartDeleteView
+
+urlpatterns = [
+    path("add", CartAddView.as_view(), name="add"),  # 购物车记录添加
+    path("", CartInfoView.as_view(), name="show"),  # 购物车记录显示
+    path("update", CartUpdateView.as_view(), name="update"),  # 购物车记录更新
+    path("delete", CartDeleteView.as_view(), name="delete"),  # 购物车记录删除
+]
